3bb759214d085f38d4e1637b412c9f1571b7c11a
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-05-09 20:54:11 +0800

    Add files via upload
    - Added VWAP indicator 
    - Finalised functionality of bot

--- Trade Bot.py
+++ Trade Bot.py
@@ -1,55 +1,29 @@
-class DeterminedYellowMonkey(QCAlgorithm):
+# - changed 4% at some point within the last 1.5 hours (high and low could be at any point within the last 1.5 hours)
+# - Draw a line at 50% fibonacchi retracement (half way between low and high)
+# - Draw a line at 61.8% fibonacchi retracement
+# - Test using options, buying ATM calls/puts with expiries 7 - 14 days, taking profit at 5, 10, 15, 20, 25 and 50% profit
+# - test 1: If price retraces to this band at some point during the day and touches vwap buy (vwap acts as support/resistance)
+# - test 2: If price retraces to this band at some point during the day and is above/below vwap depending then buy
+# - test 3: If price retraces to 50% fib at some point during the day buy, buy double if supported by vwap
+# - also test selling once price returns back to vwap or at least setting the stop at vwap once it reaches
+# - trailing stop 5%, 6%.... 10%
+# - only buy once 
+# - don't buy within last 2 hours of day
+# - sell at eod (don't carry positions over night)
+# - after a trade is made time out for 90 mins
+
+class CreativeBrownViper(QCAlgorithm):
 
     def Initialize(self):
         self.SetStartDate(2020, 1, 1)  # Set Start Date
-        self.SetStartDate(2022, 4, 4)
+        self.SetEndDate(2021, 1, 1) # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
+        self.amd = self.AddEquity("amd", Resolution.Minute).Symbol
+        self.goog = self.AddEquity("goog", Resolution.Minute).Symbol
+        self.AMD_VWAP = self.VWAP("AMD", 90)
+        self.GOOG_VWAP = self.VWAP("GOOG", 90)
         
-        self.gme = self.AddEquity("GME", Resolution.Minute).Symbol
-        
-        self.entryTicket = None
-        self.stopMarketTicket = None
-        self.entryTime = datetime.min
-        self.stopMarketOrderFillTime = datetime.min
-        self.highestPrice = 0
-
-
     def OnData(self, data: Slice):
-        if (self.Time - self.stopMarketOrderFillTime).days < 30: # wait 30 days after trade to buy again
-            return
+        self.History([self.Symbol("AMD"), self.Symbol("GOOG")], 90, Resolution.Minute) # get the price each minute of the last 90 minutes
         
-        price = self.Securities[self.gme].Price
         
-        # send limit order if stock not owned and no other orders are waiting
-        if not self.Portfolio.Invested and not self.Transactions.GetOpenOrders(self.gme):
-            quantity = self.CalculateOrderQuantity(self.gme, 0.9) # allocates 90% of portfolio to stock
-            self.entryTicket = self.LimitOrder(self.gme, quantity, price, "Entry Order") # place limit order
-            self.entryTime = self.Time
-            
-        # move price if not filled after 1 day
-        if (self.Time - self.entryTime).days > 1 and self.entryTicket.Status != OrderStatus.Filled:
-            self.entryTime = self.Time
-            updateFields = UpdateOrderFields()
-            updateFields.LimitPrice = price
-            self.entryTicket.Update(updateFields)
-            
-        # move up trailing stop loss
-        if self.stopMarketTicket is not None and self.Portfolio.Invested:
-            if price > self.highestPrice:
-                self.highestPrice = price
-                updateFields = UpdateOrderFields()
-                updateFields.StopPrice = price * 0.95
-                self.stopMarketTicket.Update(updateFields)
-        
-    def OnOrderEvent(self, orderEvent):
-        if orderEvent.Status != OrderStatus.Filled:
-            return
-        
-        # send stop loss order if entry limit order is filled
-        if self.entryTicket is not None and self.entryTicket.OrderId == orderEvent.OrderId:
-            self.stopMarketTicket = self.StopMarketOrder(self.gme, -self.entryTicket.Quantity, 0.95 * self.entryTicket.AverageFillPrice)
-            
-        # save fill time of stop loss order
-        if self.stopMarketTicket is not None and self.stopMarketTicket.OrderId == orderEvent.OrderId:
-            self.stopMarketORderFillTime = self.Time
-            self.highestPrice = 0
