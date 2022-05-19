ad9b957362b645a2caffd01ae6cd13ac9c619dfb
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-05-19 15:43:47 +0800

    Add files via upload
    - Added VWAP indicator
    - Created VWAP daily reset function
    - Added min/max indicators of price over last 2 hours

--- Trade Bot.py
+++ Trade Bot.py
@@ -11,28 +11,43 @@ class WellDressedAsparagusKoala(QCAlgorithm):
         equity.SetDataNormalizationMode(DataNormalizationMode.Raw) # options only support raw data
         self.equity = equity.Symbol
         self.SetBenchmark(self.equity) # set stock as the benchmark
+        self.vwap = self.VWAP(self.equity, 390)
         
         option = self.AddOption("AMD", Resolution.Minute)
         option.SetFilter(-2, 2, timedelta(7), timedelta(14)) # get options contracts within 2 strikes that expire between 7 and 14 days from today
         
-        self.high = self.MAX(self.equity, 21, Resolution.Daily, Field.High) # highest price over the last month
+        self.high = self.MAX(self.equity, 120) # highest price in last 120 mins
+        self.low = self.MIN(self.equity, 120) # lowest price in last 120 mins
+        
+        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.At(16, 00, 00), Action(self.Resetting)) # at the end of the day reset vwap data
 
     
     def OnData(self, data: Slice):
-        if not self.high.IsReady: # if high indicator is not ready to use return until it is
+        if not self.high.IsReady or not self.low.IsReady: # if high or low indicator is not ready to use return until it is
             return
 
         option_invested = [x.Key for x in self.Portfolio if x.Value.Invested and x.Value.Type == SecurityType.Option] # check if already invested
         
-        if option_invested:
-            if self.Time + timedelta(4) > option_invested[0].ID.Date: # if there are 4 days left to expiration sell contract
-                self.Liquidate(option_invested[0], "Too close to expiration")
-            return
+        #if option_invested:
+        #    if self.Time + timedelta(4) > option_invested[0].ID.Date: # if there are 4 days left to expiration sell contract
+        #        self.Liquidate(option_invested[0], "Too close to expiration")
+        #    return
         
-        if self.Securities[self.equity].Price >= self.high.Current.Value: # if the price has reached the highest of last month
+        #if self.Securities[self.equity].Price >= self.high.Current.Value: # if the price has reached the highest of last month
+        #    for i in data.OptionChains:
+        #        chains = i.Value
+        #        self.BuyCall(chains)
+                
+        CurrentPrice = self.Securities[self.equity].Price # get the current price of stock
+        
+        if CurrentPrice == self.vwap.Current.Value: # if the price has reached the highest of last month
             for i in data.OptionChains:
                 chains = i.Value
                 self.BuyCall(chains)
+        
+        if option_invested:        
+            if CurrentPrice >= self.BuyIn * 1.02 or CurrentPrice <= self.BuyIn * 0.99: # if stock price decreases by 1% or increases by 2% sell contract
+                self.Liquidate() # need to make it specific to the contract in question or ticker
                 
     def BuyCall(self, chains):
         expiry = sorted(chains, key = lambda x: x.Expiry, reverse = True)[0].Expiry # get the furthest expiration date (greater than 7, less than 14)
@@ -46,8 +61,12 @@ class WellDressedAsparagusKoala(QCAlgorithm):
         quantity = self.Portfolio.TotalPortfolioValue / self.call.AskPrice 
         quantity = int(0.05 * quantity / 100) # invest 5% of portfolio in option
         self.Buy(self.call.Symbol, quantity) # buy the contract
+        self.BuyIn = CurrentPrice # set record of price the of the stock when the contract was purchased (need to work out option price)
         
     def OnOrderEvent(self, orderEvent): # handle options being assigned
         order = self.Transactions.GetOrderById(orderEvent.OrderId)
         if order.Type == OrderType.OptionExercise:
             self.Liquidate()
+            
+    def Resetting(self):
+        self.vwap.Reset()
